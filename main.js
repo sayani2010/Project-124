07614b97deb22b9c33b27b8e65cfa0ebf7a7174d
sayani2010 <85400495+sayani2010@users.noreply.github.com>
2022-01-26 16:34:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup ()
+{
+    video = createCapture(VIDEO);
+    video.size(550 , 500 );
+
+    canvas = createCanvas(550 , 500);
+    canvas.position(560 , 150);
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose', gotposes);
+}
+ function draw ()
+ {
+     background('#969A97');
+ }
+  
+  function modelLoaded ()
+  {
+      console.log('poseNet is initilized');
+
+  }
+   function gotposes (results)
+   {
+       if(results.length > 0 )
+       {
+         console.log(results);
+       }
+   }
+ 
